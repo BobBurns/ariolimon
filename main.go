1c9c0e863a995aae1e6cc1382625516840af0913
Bob Burns <reburns@protonmail.com>
2017-05-31 17:02:39 -0700

    changing host ip and threshold file

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 func main() {
 
 	router := mux.NewRouter()
-	sub := router.Host("localhost").Subrouter()
+	sub := router.Host("128.114.97.100").Subrouter()
 	sub.PathPrefix("/html/").Handler(http.StripPrefix("/html/", http.FileServer(http.Dir("html"))))
 	sub.HandleFunc("/", devHandler)
 
